260d034d24b9b94ae9f6bbaf41c47eb466ca5716
Rafael Carvalho <rapha.pse@outlook.com>
2020-08-04 10:52:19 -0300

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -20,7 +20,7 @@ export class Server {
     mongoose.set('useNewUrlParser', true);
     mongoose.set('useFindAndModify', false);
     mongoose.set('useCreateIndex', true);
-    return mongoose.connect('mongodb://inventorydb:q1w2e3r4@ds143738.mlab.com:43738/heroku_glgx2dmp');
+    return mongoose.connect('mongodb://heroku_813dtxld:1ra9p9s0uv0agl77fs9sptm9ip@ds139781.mlab.com:39781/heroku_813dtxld');
   }
 
   // start router
